7c76ff994155b0583daceed2ba0fbb43d5e3fbd6
ryans <ryans@jlab.org>
2020-09-07 13:05:22 -0400

    Attempting to add test cases

---
+++ src/test/java/org/jlab/epics2web/SimpleTest.java
@@ -0,0 +1,10 @@
+package org.jlab.epics2web;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+    @Test
+    public void hello() throws Exception {
+        System.out.println("Hello");
+    }
+}
